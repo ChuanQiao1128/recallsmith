0393a7cad97f85990502aa42c3b7d8d2854ef3dc
ChuanQiao1128 <qiaochua@myvuw.ac.nz>
2026-04-07 17:07:48 +1200

    delete node.js version of lambda

--- src/public/billing/webhookApple.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-async function handleWebhookApple({ method, res }) {
-  if (method !== "POST") return res.methodNotAllowed("Method not allowed");
-  return res.notImplemented("TODO: Apple webhook verify signature + apply entitlements via internal route");
-}
-
-module.exports = { handleWebhookApple };
--- src/public/billing/webhookGoogle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-async function handleWebhookGoogle({ method, res }) {
-  if (method !== "POST") return res.methodNotAllowed("Method not allowed");
-  return res.notImplemented("TODO: Google RTDN webhook + apply entitlements via internal route");
-}
-
-module.exports = { handleWebhookGoogle };
